92c556b5350d43ad74e0db6bbfe6d18caab053ef
bhgfdfvbf <brahimamkhichan2505@gmail.com>
2025-05-02 01:54:42 +0200

    tetstt

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -13,3 +13,6 @@ Route::get('/api/hello', function () {
 Route::get('/api/greet/{name}', function ($name) {
     return response()->json(['message' => 'Hello, ' . $name . '!']);
 });
+Route::get('/api/greet/{name}/{age}', function ($name, $age) {
+    return response()->json(['message' => 'Hello, ' . $name . '! You are ' . $age . ' years old.']);
+});
